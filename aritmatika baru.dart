e89fe4487c68843aa8cd79a9bbe96dde5ea6a2f4
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-07 11:35:19 +0700

    Create aritmatika baru.dart

---
+++ aritmatika baru.dart
@@ -0,0 +1,17 @@
+//INI ADALAH OPERATOR ARITMATIKA
+ void main(){
+  var hasil1 = 20 + 20;
+  var hasil2 = 10 - 3;
+  var hasil3 = -10;
+  var hasil4 = 19 * 3;
+  var hasil5 = 12 / 6;
+  var hasil6 = 19 ~/ 6;
+  var hasil7 = 20 % 5;
+  print(hasil1);
+  print(hasil2);
+  print(hasil3);
+  print(hasil4);
+  print(hasil5);
+  print(hasil6);
+  print(hasil7);
+ }
